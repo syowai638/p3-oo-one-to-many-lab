7baea7229af5c336accaeb8f7bee53b6625e7001
syowai638 <cmuthini77@gmail.com>
2025-04-10 19:52:11 +0300

    owner.py

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,32 @@
-class Pet:
-    pass
-
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("Argument must be an instance of Pet")
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)
+
+
+class Pet:
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        if pet_type not in Pet.PET_TYPES:
+            raise Exception(f"{pet_type} is not a valid pet type")
+
+        self.name = name
+        self.pet_type = pet_type
+
+        if owner is not None and not isinstance(owner, Owner):
+            raise Exception("Owner must be an instance of Owner")
+
+        self.owner = owner
+        Pet.all.append(self)
